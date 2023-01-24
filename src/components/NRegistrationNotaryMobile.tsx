4d4739b548074a9fc4a6bf924ece6cda907830de
ebixebimelba <123515267+ebixebimelba@users.noreply.github.com>
2023-01-24 15:05:49 -0800

    Initial commit

---
+++ src/components/NRegistrationNotaryMobile.tsx
@@ -0,0 +1,199 @@
+import Shape from "assets/Shape";
+import Logo from "assets/Logo";
+import IconsBurgerMenu from "components/IconsBurgerMenu";
+import IconsCheckMark from "components/IconsCheckMark";
+import Group from "assets/Group";
+import Group1 from "assets/Group1";
+import Vector2 from "assets/Vector2";
+import Vector3 from "assets/Vector3";
+import Vector4 from "assets/Vector4";
+import Vector5 from "assets/Vector5";
+import Vector6 from "assets/Vector6";
+import Vector7 from "assets/Vector7";
+import Vector8 from "assets/Vector8";
+import Buttons from "components/Buttons";
+import Footer from "components/Footer";
+
+export default function NRegistrationNotaryMobile(
+  props: NRegistrationNotaryMobileProps
+) {
+  return (
+    <div
+      className={`relative bg-white gap-2.5 w-[375px] h-[1777px] overflow-clip font-['Poppins']`}
+      style={props.style}
+    >
+      <div
+        className="inset-x-0 top-0 absolute h-[977px] bg-[rgba(8,37,68,1)]"
+       />
+      <div
+        className="[background:url(https://uortjlczjmucmpaqqhqm.supabase.co/storage/v1/object/public/firejet-converted-images/images/06bea2f9a8bde6f1b40f833a34aa670d35a619a4.webp)_center_/_cover] absolute rounded-md w-[345px] h-[205px] top-[668px] right-[15px]"
+       />
+      <div
+        className="w-16 h-16 absolute top-[739px] right-[156px] bg-[rgba(48,144,247,1)] rounded-[70px]"
+       />
+      <Shape />
+      <Logo />
+      <IconsBurgerMenu />
+      <div
+        className="absolute bg-white rounded w-[345px] h-[512px] top-[132px] left-[calc(50%_-_172.5px_+_0px)]"
+       />
+      <div
+        className="[box-shadow:0px_0px_0px_1px_rgba(71,_190,_125,_1)_inset] [box-shadow-width:1px] h-20 absolute rounded-md w-[313px] left-[31px] top-[332px] bg-[rgba(71,190,125,0.1)]"
+       />
+      <div
+        className="w-4 h-4 absolute rounded-full left-[23px] top-[364px] bg-[rgba(71,190,125,1)]"
+       />
+      <IconsCheckMark />
+      <div
+        className="w-12 h-12 absolute bg-white rounded-md left-[50px] top-[436px]"
+       />
+      <p
+        className="h-6 absolute text-sm font-semibold leading-6 text-left inline m-0 w-[225px] left-[103px] top-[340px] text-[rgba(8,37,68,1)]"
+      >
+        Consumers pay for signings
+      </p>
+      <p
+        className="h-10 absolute text-xs font-medium text-left inline m-0 w-[225px] left-[103px] top-[364px] leading-[normal] text-[rgba(71,190,125,1)]"
+      >
+        Independent Notary gets paid for signings
+      </p>
+      <Group />
+      <div
+        className="[box-shadow:0px_0px_0px_1px_rgba(228,_230,_239,_1)_inset] [box-shadow-width:1px] absolute bg-white rounded-md w-[313px] h-[104px] left-[31px] top-[420px]"
+       />
+      <Group1 />
+      <p
+        className="h-12 absolute text-sm font-semibold text-left inline m-0 w-[225px] left-[103px] top-[428px] leading-[normal] text-[rgba(8,37,68,1)]"
+      >
+        {"An Organization will pay for signings "}
+      </p>
+      <p
+        className="h-10 absolute text-xs font-medium text-left inline m-0 w-[225px] left-[103px] top-[476px] leading-[normal] text-[rgba(71,190,125,1)]"
+      >
+        One or more Notaries perform services for an Organization
+      </p>
+      <div
+        className="absolute bg-white rounded w-[345px] h-[664px] top-[921px] left-[calc(50%_-_172.5px_+_0px)] drop-shadow-lg"
+       />
+      <p
+        className="h-6 absolute text-sm font-semibold text-left text-black inline m-0 w-[287px] left-[31px] top-[993px] leading-[25px]"
+      >
+        Lorem ipsum dolor sit amet?
+      </p>
+      <p
+        className="h-6 absolute text-sm font-semibold text-left text-black inline m-0 w-[287px] left-[31px] top-[1233px] leading-[25px]"
+      >
+        Lorem ipsum dolor?
+      </p>
+      <Vector2 />
+      <p
+        className="h-12 absolute text-sm font-semibold text-left text-black inline m-0 w-[287px] left-[31px] top-[1289px] leading-[25px]"
+      >
+        Lorem ipsum dolor sit amet, consectetuer?
+      </p>
+      <p
+        className="h-6 absolute text-sm font-semibold text-left text-black inline m-0 w-[287px] left-[31px] top-[1369px] leading-[25px]"
+      >
+        Lorem ipsum dolor?
+      </p>
+      <p
+        className="h-6 absolute text-sm font-semibold text-left text-black inline m-0 w-[287px] left-[31px] top-[1537px] leading-[25px]"
+      >
+        Lorem ipsum dolor?
+      </p>
+      <p
+        className="h-6 absolute text-sm font-semibold text-left text-black inline m-0 w-[287px] left-[31px] top-[1425px] leading-[25px]"
+      >
+        Lorem ipsum dolor sit amet?
+      </p>
+      <Vector3 />
+      <p
+        className="h-6 absolute text-sm font-semibold text-left text-black inline m-0 w-[287px] left-[31px] top-[1481px] leading-[25px]"
+      >
+        Lorem ipsum dolor sit amet?
+      </p>
+      <Vector4 />
+      <Vector5 />
+      <Vector6 />
+      <Vector7 />
+      <Vector8 />
+      <div
+        className="h-0 absolute w-[313px] left-[31px] top-[1409px] outline outline-1 outline-[rgba(228,230,239,1)]"
+       />
+      <div
+        className="h-0 absolute w-[313px] left-[31px] top-[1273px] outline outline-1 outline-[rgba(228,230,239,1)]"
+       />
+      <p
+        className="absolute text-sm font-normal text-left inline m-0 w-[313px] left-[31px] top-[1025px] leading-[22px] text-[rgba(126,130,152,1)]"
+      >
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
+      </p>
+      <div
+        className="h-0 absolute w-[313px] left-[31px] top-[1465px] outline outline-1 outline-[rgba(228,230,239,1)]"
+       />
+      <div
+        className="h-0 absolute w-[313px] left-[31px] top-[1217px] outline outline-1 outline-[rgba(228,230,239,1)]"
+       />
+      <div
+        className="h-0 absolute w-[313px] left-[31px] top-[1353px] outline outline-1 outline-[rgba(228,230,239,1)]"
+       />
+      <div
+        className="h-0 absolute w-[313px] left-[31px] top-[1521px] outline outline-1 outline-[rgba(228,230,239,1)]"
+       />
+      <p
+        className="h-6 absolute text-lg font-semibold text-center text-black inline m-0 w-[313px] top-[945px] left-[calc(50%_-_156.5px_+_0px)] leading-[normal]"
+      >
+        Common Questions
+      </p>
+      <div
+        className="opacity-50 h-1 absolute rounded w-[122px] top-[124px] left-[calc(50%_-_61px_+_-36.5px)] drop-shadow-lg bg-[rgba(71,190,125,1)]"
+       />
+      <p
+        className="h-4 absolute font-semibold text-left inline m-0 w-[122px] left-[90px] top-[104px] right-[163px] text-[10px] leading-[normal] text-[rgba(71,190,125,1)]"
+      >
+        Step 2
+      </p>
+      <div
+        className="h-1 absolute rounded w-[72px] top-[124px] left-[calc(50%_-_36px_+_-136.5px)] drop-shadow-lg bg-[rgba(71,190,125,1)]"
+       />
+      <div
+        className="h-1 absolute rounded w-[72px] top-[124px] left-[calc(50%_-_36px_+_62.5px)] drop-shadow-lg bg-[rgba(124,154,187,1)]"
+       />
+      <div
+        className="h-1 absolute rounded w-[72px] top-[124px] left-[calc(50%_-_36px_+_136.5px)] drop-shadow-lg bg-[rgba(124,154,187,1)]"
+       />
+      <p
+        className="absolute font-semibold text-center text-black inline m-0 h-[120px] w-[313px] left-[31px] top-[188px] text-[26px] leading-[normal]"
+      >
+        Who will pay for the online notary services you will provide?
+      </p>
+      <Buttons />
+      <p
+        className="absolute text-base font-medium text-center inline m-0 w-[313px] left-[31px] top-[156px] tracking-[11px] leading-[normal] text-[rgba(0,158,247,1)]"
+      >
+        SELECT PLAN
+      </p>
+      <Footer />
+    </div>
+  );
+}
+
+NRegistrationNotaryMobile.defaultProps = {
+  style: {},
+};
+
+interface NRegistrationNotaryMobileProps {
+  style: Object;
+}
+
+/**
+ * This component was generated from Figma with FireJet.
+ * Learn more at https://www.firejet.io
+ *
+ * README:
+ * The output code may look slightly different when copied to your codebase. To fix this:
+ * 1. Include the necessary fonts. The required fonts are imported from public/index.html
+ * 2. Include the global styles. They can be found in App.css
+ *
+ * Note: Step 2 is not required for tailwind.css output
+ */
